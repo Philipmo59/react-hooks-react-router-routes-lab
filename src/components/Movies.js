2716778afe12ff9f525bd735f526420390f01284
philipmo <philipmo59@gmail.com>
2023-07-06 20:23:29 -0400

    finished

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { movies } from "../data";
 
+console.log(movies)
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesItems = movies.map(movie =>(
+    <div key={movie.title}>
+    <h2>{movie.title}</h2>
+    <h3>Time: {movie.time}</h3>
+    <ul>
+    {movie.genres.map(genre =>(
+        <li key={genre}>{genre}</li>
+    ))}
+    </ul>
+    </div>
+  ))
+
+
+  return( 
+    <div>
+      <h1>Movies Page</h1>
+      {moviesItems}
+    </div>
+  )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "grey",
+  textDecoration: "none",
+  color: "white",
+};
+
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return( 
+  <div className="navbar">
+    <NavLink to ="/" exact style={linkStyles}>Home</NavLink>
+    <NavLink to ="/Movies" exact style={linkStyles}>Movies</NavLink>
+    <NavLink to ="/Directors" exact style={linkStyles} >Directors</NavLink>
+    <NavLink to ="/Actors" exact style={linkStyles}>Actors</NavLink>
+  </div>
+  )
 }
 
 export default NavBar;
